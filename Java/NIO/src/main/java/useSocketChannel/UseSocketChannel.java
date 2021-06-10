0cb02386f394d8efcd01d50e248753d679fca6ef
闫正伟 <15226569565@163.com>
2021-06-10 14:06:17 +0800

    学习使我快乐

--- Java/NIO/src/main/java/useSocketChannel/UseSocketChannel.java
+++ Java/NIO/src/main/java/useSocketChannel/UseSocketChannel.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 
 public class UseSocketChannel {
@@ -25,7 +26,7 @@ public class UseSocketChannel {
         // 对服务的IP, PORT 进行链接
         socketChannel.connect(new InetSocketAddress("127.0.0.1", 80));
 
-        while(!socketChannel.finishConnect()){
+        while (!socketChannel.finishConnect()) {
             // 不停的自旋
         }
 
