29ba67597c9f0049d7bbc452abd8aec472f46536
闫正伟 <15226569565@163.com>
2021-06-14 23:28:20 +0800

    学习使我快乐

--- Algorithm-and-DataStructure/LeetCode/src/main/java/Leetcode374.java
+++ Algorithm-and-DataStructure/LeetCode/src/main/java/Leetcode374.java
@@ -9,14 +9,11 @@ public class Leetcode374 {
 
     // 1 2 3 4 5
     public int guessNumber(int n) {
-        if (n == 1) {
-            return guess(n);
-        }
         int min = 1;
         int max = n;
         while (min < max) {
-            int mid = (max + min) / 2;
-            if (guess(min) <= 0) {
+            int mid = min + (max - min) / 2;
+            if (guess(mid) <= 0) {
                 max = mid;
             } else {
                 min = min + 1;
