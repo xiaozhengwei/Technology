76285cfab94b1f98eebd4331cd0ffede27b1b9a9
闫正伟 <15226569565@163.com>
2021-06-22 16:59:37 +0800

    学习使我快乐

---
+++ Java/COLLECTION/src/main/java/collection/CollectionTest.java
@@ -0,0 +1,13 @@
+package collection;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
+public class CollectionTest {
+
+    public static void main(String[] args) {
+        Deque<String> deque=new ArrayDeque<>();
+    }
+
+}
