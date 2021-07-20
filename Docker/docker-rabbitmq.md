6e28cd1bbc9278a58a9f0042a0475f4dcc5f590b
闫正伟 <15226569565@163.com>
2021-07-20 21:41:24 +0800

    更新文档

--- Docker/docker-rabbitmq.md
+++ Docker/docker-rabbitmq.md
@@ -1,7 +1,10 @@
 ## RabbitMQ
 
 ```shell
-# 第一个 `rabbitmq` 为容器的名字( 自行编辑 ), 第二个 `rabbitmq` 为镜像/镜像ID( 自行编辑 )
 docker pull rabbitmq:management 
-docker run -p 5672:5672 -p 15672:15672 --name rabbitmq  `rabbitmq`
 ```
+
+```shell
+docker run -p 5672:5672 -p 15672:15672 --name rabbitmq  rabbitmq
+```
+
---
+++ Java/JUC/src/main/java/juc/threadpool/ThreadTest.java
@@ -0,0 +1,29 @@
+package juc.threadpool;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ThreadTest {
+
+    class MyThread extends Thread {
+
+        @Override
+        public void run() {
+            System.out.println("执行啦~~");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Thread main=Thread.currentThread();
+        System.out.println("start");
+        Thread thread=new Thread();
+        ThreadGroup tg=thread.getThreadGroup();
+
+        System.out.println(tg);
+        System.out.println("end");
+
+    }
+
+}
