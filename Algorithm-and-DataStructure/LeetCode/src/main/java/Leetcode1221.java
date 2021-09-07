4f6d8cb532ed4ea2396893cd48f0626ff3a1817e
闫正伟 <15226569565@163.com>
2021-09-07 22:32:24 +0800

    更新代码

---
+++ Algorithm-and-DataStructure/LeetCode/src/main/java/Leetcode1221.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+public class Leetcode1221 {
+    @Test
+    public void run() {
+        Solution solution = new Solution();
+    }
+
+    class Solution {
+        public int balancedStringSplit(String s) {
+            char[] arr = s.toCharArray();
+            int length = arr.length;
+            int d = 0;
+            int ans = 0;
+            for (char ch : arr) {
+                if (ch == 'L') {
+                    d++;
+                } else {
+                    d--;
+                }
+                if (d == 0) {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
