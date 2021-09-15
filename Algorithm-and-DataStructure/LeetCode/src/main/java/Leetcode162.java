809f8ba2ce715a043d6034c3f276e05e7cbe9cf1
闫正伟 <15226569565@163.com>
2021-09-15 21:00:23 +0800

    更新一下格式

--- Algorithm-and-DataStructure/LeetCode/src/main/java/Leetcode162.java
+++ Algorithm-and-DataStructure/LeetCode/src/main/java/Leetcode162.java
@@ -1,18 +1,20 @@
 public class Leetcode162 {
 
     class Solution {
-//        public int findPeakElement(int[] nums) {
-//            int MAX = Integer.MIN_VALUE;
-//            for (int i = 0; i < nums.length; i++) {
-//                if (nums[i] > MAX) {
-//                    MAX = nums[i];
-//                    if (i + 1 < nums.length && MAX >= nums[i + 1]) {
-//                        return i;
-//                    }
-//                }
-//            }
-//            return nums.length-1;
-//        }
+        /**
+         * public int findPeakElement(int[] nums) {
+         * int MAX = Integer.MIN_VALUE;
+         * for (int i = 0; i < nums.length; i++) {
+         * if (nums[i] > MAX) {
+         * MAX = nums[i];
+         * if (i + 1 < nums.length && MAX >= nums[i + 1]) {
+         * return i;
+         * }
+         * }
+         * }
+         * return nums.length-1;
+         * }
+         **/
 
         public int findPeakElement(int[] nums) {
             int start = 0,
