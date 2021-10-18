4f7e902b44506c9fe6a677cb526629926aabcfa3
闫正伟 <15226569565@163.com>
2021-10-18 15:29:34 +0800

    学习使我快乐

--- Kafka/kafka/src/main/java/com/utunan/kafka/listener/TestMessageListener.java
+++ Kafka/kafka/src/main/java/com/utunan/kafka/listener/TestMessageListener.java
@@ -1,7 +1,23 @@
 package com.utunan.kafka.listener;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import static java.lang.Thread.sleep;
 
 @Component
 public class TestMessageListener {
+
+    @KafkaListener(id = "test-group", topics = "test")
+    public void receiveTestMessage(ConsumerRecord<Integer, String> record) throws InterruptedException {
+        System.out.println(record);
+        sleep(1);
+    }
+
+    @KafkaListener(id = "test-group-1", topics = "test")
+    public void receiveTestMessage2(ConsumerRecord<Integer, String> record) throws InterruptedException {
+        System.out.println(record);
+        sleep(1);
+    }
 }
--- RocketMQ/install.md
+++ RocketMQ/install.md
@@ -1,12 +1,12 @@
 
 
 ```
-nohup sh bin/mqnamesrv >mqnamesrv.out &
-tail -f mqnamesrv.out
+nohup sh bin/mqnamesrv >namesrv.out &
+tail -f ~/logs/rocketmqlogs/namesrv.log
 ```
 
 ```
-nohup sh ./bin/mqbroker -c ./conf/broker.conf >mqbroker.out &
-tail -f mqbroker.out
+nohup sh ./bin/mqbroker -c ./conf/broker.conf >broker.out &
+tail -f ~/logs/rocketmqlogs/broker.log 
 ```
 
