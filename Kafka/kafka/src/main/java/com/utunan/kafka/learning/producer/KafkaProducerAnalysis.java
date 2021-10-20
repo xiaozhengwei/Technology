2445e50300b6b6f17674dda6adc66f0fac99d414
闫正伟 <15226569565@163.com>
2021-10-20 10:00:23 +0800

    学习使我快乐

---
+++ Kafka/kafka/src/main/java/com/utunan/kafka/learning/producer/KafkaProducerAnalysis.java
@@ -0,0 +1,58 @@
+package com.utunan.kafka.learning.producer;
+
+import com.utunan.kafka.learning.interceptor.ProducerInterceptorPrefix;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+public class KafkaProducerAnalysis {
+    public static final String brokerList = "kafka.utunan.com:9092";
+
+    public static final String topic = "topic-demo";
+
+    public static Properties initConfig() {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", brokerList);
+        properties.put("key.serializer",
+                StringSerializer.class.getName());
+        properties.put("value.serializer",
+                StringSerializer.class.getName());
+        properties.put("client.id", "producer.client.id.demo");
+        // 重试次数
+        properties.put(ProducerConfig.RETRIES_CONFIG, 10);
+        // 设置拦截器
+        // props.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG ,ProducerInterceptorPrefix.class.getName());
+        // 设置ack 默认值为1
+        // properties.put(ProducerConfig.ACKS_CONFIG, "0");
+        return properties;
+    }
+
+    public static void main(String[] args) {
+        Properties props = initConfig();
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        ProducerRecord<String, String> record =
+                new ProducerRecord<>(topic, "hello, Kafka , KafkaProducerAnalysis");
+        try {
+            Future<RecordMetadata> recordMetadataFuture = producer.send(record);
+            RecordMetadata recordMetadata = recordMetadataFuture.get();
+            System.out.println(recordMetadata.toString());
+
+
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e != null) {
+                        e.printStackTrace();
+                    } else {
+                        System.out.println(recordMetadata.toString());
+                    }
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
