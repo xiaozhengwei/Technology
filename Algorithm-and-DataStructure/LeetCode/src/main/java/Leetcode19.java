6c69020c4a5d5d31c34d4b1a32a732f872ac72c1
闫正伟 <15226569565@163.com>
2022-02-10 20:11:07 +0800

    学习是我快乐

--- Algorithm-and-DataStructure/LeetCode/src/main/java/Leetcode19.java
+++ Algorithm-and-DataStructure/LeetCode/src/main/java/Leetcode19.java
@@ -9,22 +9,21 @@ public class Leetcode19 {
 
 
     class Solution {
-
         public ListNode removeNthFromEnd(ListNode head, int n) {
-            ListNode slow=head;
-            ListNode quick=head;
-            for(int i=0;i<n;i++){
-                quick=quick.next;
+            ListNode dummy = new ListNode(0, head);
+            ListNode first = head;
+            ListNode second = dummy;
+            for (int i = 0; i < n; ++i) {
+                first = first.next;
             }
-            while(quick.next!=null){
-                quick=quick.next;
-                slow=slow.next;
+            while (first != null) {
+                first = first.next;
+                second = second.next;
             }
-            ListNode temp=slow.next;
-            slow.next=temp.next;
-            temp=null;
-            return head;
+            second.next = second.next.next;
+            ListNode ans = dummy.next;
+            return ans;
         }
-
     }
+
 }
--- Algorithm-and-DataStructure/LeetCode/src/main/java/ListNode.java
+++ Algorithm-and-DataStructure/LeetCode/src/main/java/ListNode.java
@@ -5,4 +5,6 @@ public class ListNode {
     ListNode(int x) {
         val = x;
     }
+
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
