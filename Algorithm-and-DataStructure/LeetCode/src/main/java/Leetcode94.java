cff151675009e0c83e31ae2a9d6b3f876b7272e0
闫正伟 <15226569565@163.com>
2022-03-22 11:10:47 +0800

    学习使我快乐

--- Algorithm-and-DataStructure/LeetCode/src/main/java/Leetcode94.java
+++ Algorithm-and-DataStructure/LeetCode/src/main/java/Leetcode94.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Leetcode94 {
 
@@ -11,22 +12,38 @@ public class Leetcode94 {
     }
 
     class Solution {
+
         public List<Integer> inorderTraversal(TreeNode root) {
+            Stack<TreeNode> stack = new Stack<>();
             List<Integer> result = new ArrayList<>();
-            find(root, result);
+            while (root != null || !stack.isEmpty()) {
+                while (root != null) {
+                    stack.push(root);
+                    root = root.left;
+                }
+                root = stack.pop();
+                result.add(root.val);
+                root = root.right;
+            }
             return result;
         }
 
-        public void find(TreeNode node, List<Integer> result) {
-            if (node != null) {
-                if (node.left != null) {
-                    find(node.left, result);
-                }
-                result.add(node.val);
-                if (node.right != null) {
-                    find(node.right, result);
-                }
-            }
-        }
+//        public List<Integer> inorderTraversal(TreeNode root) {
+//            List<Integer> result = new ArrayList<>();
+//            find(root, result);
+//            return result;
+//        }
+//
+//        public void find(TreeNode node, List<Integer> result) {
+//            if (node != null) {
+//                if (node.left != null) {
+//                    find(node.left, result);
+//                }
+//                result.add(node.val);
+//                if (node.right != null) {
+//                    find(node.right, result);
+//                }
+//            }
+//        }
     }
 }
---
+++ Linux/IPV4设置.md
@@ -0,0 +1,8 @@
+network:
+  ethernets:
+    ens32:
+      addresses: [192.168.100.101/24] 
+      dhcp4: false
+      optional: true
+      gateway4: 192.168.100.1
+  version: 2
